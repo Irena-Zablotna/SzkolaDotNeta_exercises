c1d8e882fb4f0c9b43e5f1d9f00f70f202a7a670
Irena Zablotna <irene.zablotna@waveinformatica.com>
2024-02-26 18:09:00 +0100

    Add project files.

---
+++ Module2_lesson4/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace Module2_lesson4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //EXERCISE 1
+            string name;
+            string surname;
+            int age;
+            char sex;
+            string taxId;
+            long employeeId;
+            Console.WriteLine("Write your first name");
+            name = Console.ReadLine();
+            Console.WriteLine("Write your surname");
+            surname = Console.ReadLine();
+            Console.WriteLine("Write your age");
+            age = int.Parse(Console.ReadLine());
+            Console.WriteLine(" What is your sex, you can choose \"m\" for male or \"f\" for female");
+            sex = char.Parse(Console.ReadLine());
+            Console.WriteLine("Write your tax Id");
+            taxId = Console.ReadLine();
+            Console.WriteLine("Write your employee Id");
+            employeeId = long.Parse(Console.ReadLine());
+            Console.Write($"Hello {name} {surname}!\r\n You are {age} old and your sex is {sex}.\r\n Your tax id is {taxId} " +
+                $"and your employee Id is {employeeId}. Press any key to exit");
+            Console.ReadKey();
+
+            //EXERCISE 2
+            char a = 'a';
+            char b = 'b';
+            char c = 'c';
+            Console.WriteLine( $"reverse order {c} {b} {a}");
+            Console.ReadKey();
+
+            //EXERCISE 3
+
+            Console.WriteLine("Enter the width of the rectangle:");
+            double width = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter the length of the rectangle:");
+            double length = Convert.ToDouble(Console.ReadLine());
+
+            double diagonal = Math.Sqrt(Math.Pow(width, 2) + Math.Pow(length, 2));
+            double roundedDiagonal = Math.Round(diagonal, 2);
+
+            Console.WriteLine($"The length of the diagonal of the rectangle is: {roundedDiagonal}");
+            Console.ReadKey();
+
+            //EXERCISE 4
+
+        }
+    }
+}
