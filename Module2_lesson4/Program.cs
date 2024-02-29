a21fd9d8bbe16c733ff26c942be93321e3f6b256
Irena Zablotna <irene.zablotna@waveinformatica.com>
2024-02-29 18:14:27 +0100

    added error handling

--- Module2_lesson4/Program.cs
+++ Module2_lesson4/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.VisualBasic.FileIO;
+using System;
 using System.ComponentModel;
 using System.Diagnostics.CodeAnalysis;
 using System.Globalization;
@@ -9,37 +10,67 @@ namespace Module2_lesson4
     {
         static void Main(string[] args)
         {
-            //EXERCISE 1
+            CultureInfo culture = CultureInfo.InvariantCulture;
+            //EXERCISE 1 z obsluga wyjatkow
             string name;
             string surname;
             int age;
             char sex;
-            string description;
             string taxId;
-            long employeeId;
+            string employeeId;
+            bool isValidInput = false;
 
             Console.WriteLine("Write your first name");
             name = Console.ReadLine();
             Console.WriteLine("Write your surname");
             surname = Console.ReadLine();
             Console.WriteLine("Write your age");
-            age = int.Parse(Console.ReadLine());
+
+            while (!isValidInput)
+            {
+                if (int.TryParse(Console.ReadLine(), out age))
+                {
+                    isValidInput = true;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input. Please enter a valid age as a number.");
+                }
+            }
             Console.WriteLine(" What is your sex, you can choose \"m\" for male or \"f\" for female");
-            sex = char.Parse(Console.ReadLine());
+             isValidInput = false;
+            while (!isValidInput)
+            {
+                if (char.TryParse(Console.ReadLine(), out sex))
+                {
+                    if (sex == 'f' || sex =='m')
+                    {
+                        isValidInput = true;
+                    }
+                    else
+                    {
+                        isValidInput = false;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input. Please enter only \"m\" or \"f\".");
+                }
+            }
             Console.WriteLine("Write your tax Id");
             taxId = Console.ReadLine();
             Console.WriteLine("Write your employee Id");
-            employeeId = long.Parse(Console.ReadLine());
+            employeeId = (Console.ReadLine());
             Console.ReadKey();
 
             //EXERCISE 2
             char a = 'a';
             char b = 'b';
             char c = 'c';
-            Console.WriteLine( $"reverse order {c} {b} {a}");
+            Console.WriteLine($"reverse order {c} {b} {a}");
             Console.ReadKey();
 
-            //EXERCISE 3
+            //EXERCISE 3  tu nie dawalam obslugi wyjatkow ale wiem, ze powinnam
 
             Console.WriteLine("Enter the width of the rectangle:");
             double width = Convert.ToDouble(Console.ReadLine());
@@ -61,16 +92,64 @@ namespace Module2_lesson4
             //EXERCISE 5
             Console.WriteLine("What is your name?");
             string name1 = Console.ReadLine();
+
             Console.WriteLine("Write the first letter of your surname ");
-            char firstChar = char.Parse(Console.ReadLine());
+            char firstChar = '\0';
+            isValidInput = false;
+            while (!isValidInput)
+            {
+                if (char.TryParse(Console.ReadLine(), out firstChar))
+                {
+                    isValidInput = true;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input. Please enter only one character.");
+                }
+            }
             Console.WriteLine("Write your phone number with international prefix e.g: 0012345678");
-            long phone = Convert.ToInt64(Console.ReadLine());
+            isValidInput = false;
+            long phone = 0;
+            while (!isValidInput)
+            {
+                if (long.TryParse(Console.ReadLine(), out phone))
+                {
+                    isValidInput = true;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input. Please enter only numbers.");
+                }
+            }
             Console.WriteLine("What is your weight?");
-            CultureInfo culture = CultureInfo.InvariantCulture;
-            double weight = double.Parse(Console.ReadLine(), culture);
+            isValidInput = false;
+            double weight = 0.0;
+            while (!isValidInput)
+            {
+                if (double.TryParse(Console.ReadLine(), culture, out weight))
+                {
+                    isValidInput = true;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input. Please enter a valid weight as a number.");
+                }
+            }
             Console.WriteLine("Do you like pizza? (You can answer true or false)");
+            isValidInput = false;
+            bool pizza = false;
+            while (!isValidInput)
+            {
+                if (bool.TryParse(Console.ReadLine(), out pizza))
+                {
+                    isValidInput = true;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input. Please enter only \"true\" or \"false\".");
+                }
+            }
             string str2;
-            bool pizza = bool.Parse(Console.ReadLine());
             if (pizza)
             {
                 str2 = "like pizza, me too ;-)!";
@@ -79,9 +158,11 @@ namespace Module2_lesson4
             {
                 str2 = "don't like pizza. What a pity!";
             }
-            Console.WriteLine($"Hello {name1} {firstChar}. Your phone number is {phone} and your weight is {weight}. \r\n You {str2}");
-            Console.ReadKey(); 
 
+            Console.WriteLine($"Hello {name1} {firstChar}. Your phone number is {phone} and your weight is {weight}. \r\n You {str2}");
+            Console.ReadKey();
         }
     }
 }
+
+
